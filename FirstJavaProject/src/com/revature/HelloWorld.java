9eb891174b42dc6a29f20b67b46ae1149a76f2d6
Kirk Hahn <kirkhahn11@yahoo.com>
2021-08-23 14:31:56 -0500

    add HelloWorld class

---
+++ FirstJavaProject/src/com/revature/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.revature;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		/**
+		 * this 
+		 * is a 
+		 * multi-line comment
+		 */
+		// syso + control+space === System.out.println();
+		System.out.println("Hello world... from the IDE!");
+	}
+
+}
